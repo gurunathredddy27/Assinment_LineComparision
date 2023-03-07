073413f9168a86803f67abf47dc228ca9ed70929
gurunathreddy2727@gmail.com <gurunathreddy2727@gmail.com>
2023-03-07 21:36:05 +0530

    length of the line

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
         System.out.println("Welcome to Line Comparison Computation Program!");
+
+        Scanner sc = new Scanner(System.in);
+        int x1 = sc.nextInt();
+        System.out.println("Please enter x1 number");
+        int y1 = sc.nextInt();
+        System.out.println("Please enter y1 number");
+        int x2 = sc.nextInt();
+        System.out.println("Please enter x2 number");
+        int y2 = sc.nextInt();
+        System.out.println("Please enter y2 number");
+
+        double length = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+        System.out.println("the length of the line is "+ length);
 
 
 
